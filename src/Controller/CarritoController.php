54981a46f8cb8de70b7fd46e57d0b3a5020f93eb
JavierSanApa <javi_sanchez10@outlook.com>
2024-05-22 17:28:37 +0200

    Popup del carrito falta dejarlo mejor con el css

--- src/Controller/CarritoController.php
+++ src/Controller/CarritoController.php
@@ -2,13 +2,14 @@
 // src/Controller/CarritoController.php
 namespace App\Controller;
 
-use App\Repository\ProductoRepository;
 use App\Repository\TallaRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProductoRepository;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CarritoController extends AbstractController
 {
@@ -24,46 +25,46 @@ class CarritoController extends AbstractController
         ]);
     }
 
-
     #[Route('/añadirAcarrito/{idProducto}', name: 'app_añadir_a_carrito')]
     public function añadirACarrito(Request $request, ProductoRepository $productoRepository, $idProducto, TallaRepository $tallaRepository, Security $security): Response
     {
         if (!$security->isGranted('IS_AUTHENTICATED_FULLY')) {
             $this->addFlash('error', 'Debes iniciar sesión para agregar productos al carrito.');
-            return $this->redirectToRoute('app_login'); 
+            return $this->redirectToRoute('app_login');
         }
-    
+
         $producto = $productoRepository->find($idProducto);
         $tallaId = $request->request->get('talla');
         $cantidad = intval($request->request->get('cantidad', 1));
-    
+
         $claveProductoCarrito = $idProducto . '-' . $tallaId;
-    
+
         $carrito = $request->getSession()->get('carrito', []);
-    
+
         if (isset($carrito[$claveProductoCarrito])) {
             $carrito[$claveProductoCarrito]['cantidad'] += $cantidad;
-            // Asegúrate de recalcular el subtotal cada vez que cambia la cantidad
             $carrito[$claveProductoCarrito]['subtotal'] = $carrito[$claveProductoCarrito]['cantidad'] * $carrito[$claveProductoCarrito]['precio'];
         } else {
-            // Al crear una nueva entrada en el carrito, incluye 'precio' y calcula 'subtotal'
             $carrito[$claveProductoCarrito] = [
                 'idProducto' => $idProducto,
                 'nombre' => $producto->getNombre(),
                 'cantidad' => $cantidad,
                 'tallaId' => $tallaId,
                 'tallaNombre' => $tallaRepository->find($tallaId)->getNombre(),
-                'precio' => $producto->getPrecio(), // Asume que el Producto tiene un método getPrecio()
+                'precio' => $producto->getPrecio(),
                 'subtotal' => $cantidad * $producto->getPrecio(),
             ];
         }
-    
-        $request->getSession()->set('carrito', $carrito);
-    
-        return $this->redirectToRoute('app_carrito');
-    }
-    
 
+        $request->getSession()->set('carrito', $carrito);
+
+        if ($request->isXmlHttpRequest()) {
+            return new JsonResponse(['message' => 'Producto añadido correctamente al carrito']);
+        }
+    
+        // Solo redirecciona si no es una llamada AJAX
+        return $this->redirectToRoute('ropa_page');
+    }
 
     #[Route('/eliminarDelCarrito/{claveProductoCarrito}', name: 'app_eliminar_del_carrito', methods: ['POST'])]
     public function eliminarDelCarrito(Request $request, $claveProductoCarrito): Response
@@ -76,5 +77,4 @@ class CarritoController extends AbstractController
 
         return $this->redirectToRoute('app_carrito');
     }
-
 }
