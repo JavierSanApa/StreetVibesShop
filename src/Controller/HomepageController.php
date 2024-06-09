d54008c66f8ad18109a893193ba82c02d707496b
JavierSanApa <javi_sanchez10@outlook.com>
2024-06-09 12:31:24 +0200

    Navbar arreglado y botones sesion css basico

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -11,12 +11,13 @@ use Symfony\Bundle\SecurityBundle\Security;
 class HomepageController extends AbstractController
 {   
     #[Route('/homepage', name: 'app_homepage')]
-    public function index(Security $security) {
-    
-        // Si el usuario no está autenticado, simplemente muestra la página de inicio
+    public function index(Security $security): Response {
+        // Verifica si el usuario está autenticado
+        $isAuthenticated = $security->isGranted('IS_AUTHENTICATED_FULLY');
+
+        // Pasa el estado de autenticación a la plantilla Twig
         return $this->render('homepage/index.html.twig', [
-            'isAuthenticated' => false, // Indica que el usuario no está autenticado
+            'isAuthenticated' => $isAuthenticated,
         ]);
     }
-
 }
